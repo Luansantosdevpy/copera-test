1bf6b07246226671c52c09eb9a33e3728e26080c
luansantosdevpy <luansantosdev.py@gmail.com>
2024-03-26 15:23:25 -0300

    feat: dependency to toDo service and toDo repository

--- src/dependencyContainer.ts
+++ src/dependencyContainer.ts
@@ -3,6 +3,9 @@ import Logger from './infrastructure/log/logger';
 import HealthCheckRepositoryInterface from './domain/interfaces/repositories/healthCheckRepositoryInterface';
 import PostgresHealthCheckRepository from './infrastructure/data/repositories/healthCheckRepository';
 import HealthCheckService from './application/services/healthCheckService';
+import ToDoRepositoryInterface from './domain/interfaces/repositories/toDoRepositoryInterface';
+import ToDoRepository from './infrastructure/data/repositories/toDoRepository';
+import ToDoService from './application/services/toDoService';
 
 export default async (container: DependencyContainer): Promise<void> => {
   Logger.debug('Dependency container initializing...');
@@ -16,6 +19,17 @@ export default async (container: DependencyContainer): Promise<void> => {
 
   container.register<HealthCheckService>('HealthCheckService', {
     useClass: HealthCheckService
+  });
+
+  container.register<ToDoRepositoryInterface>(
+    'ToDoRepositoryInterface',
+    {
+      useClass: ToDoRepository
+    }
+  );
+
+  container.register<ToDoService>('ToDoService', {
+    useClass: ToDoService
   });
 
   Logger.debug('Dependency container initialized!');
