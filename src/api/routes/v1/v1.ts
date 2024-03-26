ce742d9c02605e3694c23d4d6c78e83db97cc2ee
luansantosdevpy <luansantosdev.py@gmail.com>
2024-03-26 12:30:12 -0300

    feat: create healthcheck route

--- src/api/routes/v1/v1.ts
+++ src/api/routes/v1/v1.ts
@@ -1,9 +1,11 @@
 import { Router } from 'express';
+import healthCheck from './healthCheck';
 
 export default async (): Promise<Router> => {
   const router = Router();
 
   router.use('/api', router);
+  router.use('/', await healthCheck());
 
   return router;
 };
